ea08f4ae7a5bfd5b470a07c33298e517467818d6
chanmmn <33271651+chanmmn@users.noreply.github.com>
2021-04-04 00:12:12 -0700

    Commit Blazor Server call Web Api
    Commit Blazor Server call Web Api

---
+++ BlazorServerCallWebApi/WebApplWebApicore/WebApplWebApicore/Controllers/WeatherForecastController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplWebApicore.Controllers
+{
+    public class DataObject
+    {
+        public string Name { get; set; }
+    }
+
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        //public IEnumerable<WeatherForecast> Get()
+        //{
+        //    var rng = new Random();
+        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //    {
+        //        Date = DateTime.Now.AddDays(index),
+        //        TemperatureC = rng.Next(-20, 55),
+        //        Summary = Summaries[rng.Next(Summaries.Length)]
+        //    })
+        //    .ToArray();
+        //}
+        public DataObject Get()
+        {
+            //return "value";
+
+            DataObject data = new DataObject();
+            data.Name = "mm"+DateTime.Now.ToString("F");
+            //return new string[] { "value1", "value2" };
+            return data;
+        }
+
+       
+    }
+}
