c82da2444c0d1c1d4eddb3e46ef64f74676d47eb
chanmmn <33271651+chanmmn@users.noreply.github.com>
2021-04-10 23:54:58 -0700

    Commit BlazorServerCallWebApiIEnumerable
    Commit BlazorServerCallWebApiIEnumerable

---
+++ BlazorServerCallWebApiIEnumerable/WebApiBlazorServer/WebApiBlazorServer/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace WebApiBlazorServer.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
---
+++ BlazorServerCallWebApiIEnumerable/WebApiBlazorServer/WebApiBlazorServer/Controllers/ValuesController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApiBlazorServer.Controllers
+{
+    public class Customer
+    {
+        public string CustomerCode { get; set; }
+        public string CustomerName { get; set; }
+    }
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        //public IEnumerable<string> Get()
+        //public Customer Get()
+        public IEnumerable<Customer> Get()
+        {
+            List<Customer> lst = new List<Customer>
+            {
+                new Customer{CustomerCode = "1", CustomerName = "CMM" },
+                new Customer{CustomerCode = "2", CustomerName = "CYY" }
+            };
+            //Customer custom = new Customer()
+            //{ CustomerCode = "1", CustomerName = "CMM" };
+
+            //return custom;
+            return lst;
+            //return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
