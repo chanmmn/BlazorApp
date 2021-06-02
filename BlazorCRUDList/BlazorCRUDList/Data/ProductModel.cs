80510494aca1a47ba3998d69dea5c11efc73e26b
chanmmn <33271651+chanmmn@users.noreply.github.com>
2021-06-02 16:19:00 +0800

    Commit BlazorCRUDList
    Blazor CRUS

---
+++ BlazorCRUDList/BlazorCRUDList/Data/ProductModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCRUDList.Data
+{
+    public class ProductModel
+    {
+        public string ProductName { get; set; }
+        public string UnitPrice { get; set; }
+    }
+}
